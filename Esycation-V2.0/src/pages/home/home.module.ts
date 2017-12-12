ea5695aa8d8ad16027b538bf2555fceb0f0716e0
Madan Mohan Singh <madansingh18@gmail.com>
2017-12-12 11:43:48 +0530

    UI Prototype for Home Screen

--- Esycation-V2.0/src/pages/home/home.module.ts
+++ Esycation-V2.0/src/pages/home/home.module.ts
@@ -1,13 +1,49 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import {HomeComponent} from '../home/home';
+import { NgCalendarModule } from 'ionic2-calendar';
 
 @NgModule({
-    imports: [
-        IonicPageModule.forChild(HomeComponent),
-    ],
-    exports: [HomeComponent ],
-    declarations: [HomeComponent],
-    providers: [],
+ imports: [
+    NgCalendarModule,
+    IonicPageModule.forChild(HomeComponent),
+ 
+ ],
+
+ exports: [HomeComponent ],
+ declarations: [HomeComponent],
+ providers: [],
+
 })
-export class HomeModule { }
+export class HomeModule { 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { NgModule } from '@angular/core';
+// import { IonicPageModule } from 'ionic-angular';
+// import {HomeComponent} from '../home/home';
+
+// @NgModule({
+//     imports: [
+//         IonicPageModule.forChild(HomeComponent),
+//     ],
+//     exports: [HomeComponent ],
+//     declarations: [HomeComponent],
+//     providers: [],
+// })
+// export class HomeModule { }
