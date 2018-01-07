2286dd7831122262c3469026f4cde24d58a527e2
anjit4myself <anjit4myself@gmail.com>
2018-01-07 15:54:10 +0530

    model class for view notification

---
+++ Esycation-V2.0/src/providers/model/notification/notification.staff.model.ts
@@ -0,0 +1,14 @@
+export class StaffDetails{
+
+    id:number=null;
+	name:string=null;
+    gender:string=null;
+	mobile:string=null;
+	email:string=null;
+	subjects:any=null;
+	documents:any=null;
+    designationId:any=null;
+    departmentId:any=null;
+    imageId:number=null;
+    
+}
