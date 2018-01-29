03aad623e23920cf5c7c603e41ab952d89b65e62
unknown <anjit4myself@gmail.com>
2018-01-29 15:09:30 +0530

    change guardian dashbaord

--- Esycation-V2.0/src/providers/service/staff/staff.service.ts
+++ Esycation-V2.0/src/providers/service/staff/staff.service.ts
@@ -1,29 +1,35 @@
 
-import {Injectable,Inject} from '@angular/core';
-import {Http} from '@angular/http';
+import { Injectable, Inject } from '@angular/core';
+import { Http } from '@angular/http';
 import { ServerConfig } from '../../config';
-import {Observable} from 'rxjs/Rx';
+import { Observable } from 'rxjs/Rx';
 import { CostumErrorHandler } from '../core/error.service';
-import {BaseService} from '../core/base.service';
-import {CommonServices} from '../common/common.service';
-import {UserSessionService} from '../../service/core/user.session.service';
-import {Staff} from '../../model/staff/model.staff';
+import { BaseService } from '../core/base.service';
+import { CommonServices } from '../common/common.service';
+import { UserSessionService } from '../../service/core/user.session.service';
+import { Staff } from '../../model/staff/model.staff';
 
 @Injectable()
 export class StaffService extends BaseService<Staff>{
 
-    constructor(@Inject(Http) protected http: Http,
+  constructor( @Inject(Http) protected http: Http,
     @Inject(CostumErrorHandler) protected errorHandler: CostumErrorHandler,
-    @Inject(UserSessionService) protected userSessionService:UserSessionService,
-    @Inject (CommonServices) protected commonServices:CommonServices){
-      super(http,errorHandler);
-   }
+    @Inject(UserSessionService) protected userSessionService: UserSessionService,
+    @Inject(CommonServices) protected commonServices: CommonServices) {
+    super(http, errorHandler);
+  }
 
-   findByDepartmentIds(departmentId:number,viewName:string):Observable<any>{
+  findByDepartmentIds(departmentId: number, viewName: string): Observable<any> {
 
-    let url: string = ServerConfig.getPath() 
-    +"/staffs/findByDepartmentIds/"+departmentId+"?RESPONSE_VIEW="+viewName+"&sort=name&page=1&&size=1000"; 
+    let url: string = ServerConfig.getPath()
+      + "/staffs/findByDepartmentIds/" + departmentId + "?RESPONSE_VIEW=" + viewName + "&sort=name&page=1&&size=1000";
     return this.findAll(url);
 
-   }
+  }
+
+  public findClassTeacherByStudentId(studentId: number): Observable<any> {
+    let url: string = ServerConfig.getPath() + "/staffs/findClassTeacherByStudentId/" + studentId + "?RESPONSE_VIEW=Staff.MinDetails";
+    return this.findAll(url);
+  }
+
 }
